16a883b7aaf107ef546474c7e623575dea82b119
HBG <victor@ubuntu.ubuntu-domain>
2013-07-11 03:02:03 +0300

    Added highscore statistics Improved apple's new coordinates calculation Added kind of 3-d effects to the snake, walls and apples

--- src/snake2/core.clj
+++ src/snake2/core.clj
@@ -1,9 +1,35 @@
 (ns snake2.core
   (:import (java.awt Color Dimension)
-           (javax.swing JPanel JFrame Timer JOptionPane)
+           (javax.swing JPanel JFrame Timer JOptionPane JLabel)
            (java.awt.event ActionListener KeyListener KeyEvent))
   (:use clojure.java.io))
 
+
+;Constants
+
+(def highscore-filename "snake_highscore.txt")
+
+(def board-width 50)
+(def board-height 25)
+(def point-size 15)
+(def game-speed-millis 75)
+(def win-length 10)
+(def dirs {KeyEvent/VK_LEFT [-1 0]
+           KeyEvent/VK_RIGHT [1 0]
+           KeyEvent/VK_UP [0 -1]
+           KeyEvent/VK_DOWN [0 1]})
+
+(def points-per-apple 1)
+(def lv2-score (* 10 points-per-apple))
+(def lv3-score (* 10 points-per-apple))
+(def current-score (ref 0))
+
+(defn create-board-coords []
+  (for [x (range board-width)
+        y (range board-height)]
+    [x y]))
+
+(def board-coords (create-board-coords))
 
 ;Work with files
 
@@ -17,17 +43,11 @@
   (with-open [r (java.io.PushbackReader. (reader filename))]
     (read r)))
 
-;Constants
-
-(def board-width 75)
-(def board-height 50)
-(def point-size 10)
-(def game-speed-millis 75)
-(def win-length 5)
-(def dirs {KeyEvent/VK_LEFT [-1 0]
-           KeyEvent/VK_RIGHT [1 0]
-           KeyEvent/VK_UP [0 -1]
-           KeyEvent/VK_DOWN [0 1]})
+(defn save-highscore [frame]
+  (let [highscore (deserialize highscore-filename)]
+    (when (> @current-score highscore)
+      (serialize @current-score highscore-filename)
+      (JOptionPane/showMessageDialog frame (str "New Highscore: " @current-score)))))
 
 ;Used to calculate the new position of a moving game obj
 (defn add-points [& pts]
@@ -39,10 +59,11 @@
   (map #(* point-size % )
        [(pt 0) (pt 1) 1 1]))
 
-(defn create-apple []
-  {:location [(rand-int board-width) (rand-int board-height)]
-   :color (Color. 210 50 80)
-   :type :apple})
+(defn create-apple [{walls-coords :coordinates}]
+  (let [availabe-coords (remove (set walls-coords) board-coords)]
+    {:location (rand-nth availabe-coords)
+     :color (Color. 210 50 80)
+     :type :apple}))
 
 (defn create-snake []
   {:body (list [1 1])
@@ -78,37 +99,49 @@
   (or (head-crashed-body? snake)
     (head-crashed-wall? snake)))
 
-(defn eats-apple? [{[head] :body} {apple-location :location}]
+(defn eat-apple? [{[head] :body} {apple-location :location}]
   (= head apple-location))
 
 (defn turn-snake [snake newdir]
   (assoc snake :dir newdir))
 
 ;The mutable model with STM
-(defn reset-game [snake apple]
+(defn reset-game [snake apple walls]
 ;TODO read more about the refs
-  (dosync (ref-set apple (create-apple))
-          (ref-set snake (create-snake)))
+  (dosync (ref-set apple (create-apple walls))
+          (ref-set snake (create-snake))
+          (ref-set current-score 0))
   nil)
 
 (defn update-dir [snake newdir]
   (when newdir
     (dosync (alter snake turn-snake newdir))))
 
-(defn update-positions [snake apple]
+(defn update-positions [snake apple walls]
   (dosync
-    (if (eats-apple? @snake @apple)
-      (do (ref-set apple (create-apple))
-           (alter snake move :growfat))
+    (if (eat-apple? @snake @apple)
+      (do (ref-set apple (create-apple walls))
+          (alter snake move :growfat)
+          (alter current-score + points-per-apple))
       (alter snake move)))
    nil)
 
 ;GUI
 
+(def border-size 3)
 (defn fill-point [g pt color]
   (let [[x y width height] (convert-point-to-screen-rect pt)]
         (.setColor g color)
-        (.fillRect g x y width height)))
+        (.fillRect g x y width height)
+;  Draw Shadows for more realistic 3-D effect
+        (.setColor g (.brighter color))
+        (.fillRect g x y width border-size)
+        (.fillRect g x y border-size height)
+        (.setColor g (.darker color))
+        (.fillRect g x (- y border-size) width border-size)
+        (.fillRect g (- x border-size) y border-size height)
+        (.fillRect g x (+ y height) width border-size)
+        (.fillRect g (+ x width) y border-size height)))
 
 (defmulti paint (fn [g object & _] (:type object)))
 
@@ -124,23 +157,27 @@
     (fill-point g point color)))
 
 
-(defn game-board [frame snake apple walls]
+(defn game-board [frame snake apple walls scorelabel]
 ;TODO read more about "proxy"
   (proxy [JPanel ActionListener KeyListener] []
     (paintComponent [g]
       (proxy-super paintComponent g)
       (paint g @snake)
       (paint g @apple)
-      (paint g walls))
+      (paint g @walls))
     (actionPerformed [e]
       ;actionPerformed is called on every timer tick
-      (update-positions snake apple)
+      (update-positions snake apple walls)
+      (prn @current-score)
+;      (.setText scorelabel (str "Score: " @current-score))
       (when (lose-game? @snake)
-        (reset-game snake apple)
-        (JOptionPane/showMessageDialog frame "You lose!"))
+        (JOptionPane/showMessageDialog frame "You lose! Try your skills again.")
+        (save-highscore frame)
+        (reset-game snake apple walls))
       (when (win? @snake)
-        (reset-game snake apple)
-        (JOptionPane/showMessageDialog frame "You win!"))
+        (JOptionPane/showMessageDialog frame "You win!")
+        (save-highscore frame)
+        (reset-game snake apple walls))
       (.repaint this))
     (keyPressed [e]
       (update-dir snake (dirs (.getKeyCode e))))
@@ -151,11 +188,12 @@
     (keyTyped [e])))
 
 (defn game []
-  (let [snake (ref (create-snake))
-        apple (ref (create-apple))
-        walls (create-walls)
+  (let [walls (atom (create-walls))
+        snake (ref (create-snake))
+        apple (ref (create-apple walls))
         frame (JFrame. "Clojure Snake")
-        board (game-board frame snake apple walls)
+        scorelabel (JLabel. "Score: 0")
+        board (game-board frame snake apple walls scorelabel)
         timer (Timer. game-speed-millis board)]
     (doto board
       (.setFocusable true)
@@ -163,11 +201,9 @@
     (doto frame
       (.add board)
       (.pack)
-      (.setVisible true))
+      (.setVisible true)
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE))
     (.start timer)
-
-;    [snake apple timer]
-
 ))
 
 (defn -main [& args]
